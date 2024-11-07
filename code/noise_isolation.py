081e6edfc4e37b8e6676fe9771e2f4f423d365f6
root <Yunseo>
2024-11-07 10:17:05 +0000

    feat: re-factoring NI

--- code/noise_isolation.py
+++ code/noise_isolation.py
@@ -4,70 +4,60 @@ import os
 import logging
 
 class Noise_isolation:
-    def __init__(self, input_file, output_file, non_korean_ratio_lower_threshold=0.1, non_korean_ratio_upper_threshold=0.3):
-        """
-        Noise_isolation
-     클래스는 주어진 CSV 파일에서 텍스트 데이터를 처리하여
-        한국어가 아닌 문자 비율을 계산하고, 지정된 비율 범위에 해당하는 데이터를 필터링합니다.
-
-        :param input_file: 입력 CSV 파일 경로
-        :param output_file: 출력 CSV 파일 경로
-        :param non_korean_ratio_lower_threshold: 한국어가 아닌 문자 비율 하한값
-        :param non_korean_ratio_upper_threshold: 한국어가 아닌 문자 비율 상한값
-        """
-        # 로깅 설정
+    def __init__(self, input_file, output_file):
         logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
         self.input_file = input_file
         self.output_file = output_file
-        self.non_korean_ratio_lower_threshold = non_korean_ratio_lower_threshold
-        self.non_korean_ratio_upper_threshold = non_korean_ratio_upper_threshold
+
         self.pattern = r'(?<![A-Z])[A-Z](?![A-Z])|[^ㄱ-ㅎ가-힣\u4E00-\u9FFF0-9\sA-Z\u2026·∼]'
 
         self.df = None  # 원본 데이터프레임
         self.filtered_df = None  # 필터링된 데이터프레임
 
+    # 주어진 문자열에서 '...'을 '…'으로 대체합니다.
     def replace_ellipsis(self, text):
-        """
-        주어진 문자열에서 '...'을 '…'으로 대체합니다.
-
-        :param text: 입력 문자열
-        :return: 변환된 문자열
-        """
         if isinstance(text, str):
             return text.replace('...', '…')
         return text
-
+    
+    # 주어진 문자열에서 정규 표현식 패턴에 매칭되는 문자들만 추출하여 반환합니다.
     def extract_non_korean_chars(self, text):
-        """
-        주어진 문자열에서 정규 표현식 패턴에 매칭되는 문자들만 추출하여 반환합니다.
-
-        :param text: 입력 문자열
-        :return: 추출된 문자들의 문자열
-        """
         if isinstance(text, str):
             matches = re.findall(self.pattern, text)
             return ''.join(matches)
         return ''
-
-    def calculate_non_korean_ratio_vectorized(self):
-        """
-        벡터화된 방법으로 한국어가 아닌 문자 비율을 계산합니다.
-        """
-        # 'non_korean_count'는 'non_kor' 열의 길이로 계산
-        self.df['non_korean_count'] = self.df['non_kor'].str.len()
-        # 전체 문자 수 계산
-        self.df['total_chars'] = self.df['text'].str.len()
-        # 비율 계산
-        self.df['non_korean_ratio'] = (self.df['non_korean_count'] / self.df['total_chars']).round(4)
-        # 불필요한 중간 열 제거
-        self.df.drop(['non_korean_count', 'total_chars'], axis=1, inplace=True)
+    # 한국어가 아닌 문자의 비율을 계산합니다.
+    def calculate_non_korean_ratio(self):
+        self.df['non_korean_count'] = self.df['non_kor'].str.len() # 한국어가 아닌 문자 'non_kor' 열의 길이로 계산
+        
+        self.df['total_chars'] = self.df['text'].str.len() # 전체 문자 수 계산
+        
+        self.df['non_korean_ratio'] = (self.df['non_korean_count'] / self.df['total_chars']).round(4) # 비율 계산
+        
+        self.df.drop(['non_korean_count', 'total_chars'], axis=1, inplace=True) # 불필요한 중간 열 제거
         logging.info("한국어가 아닌 문자 비율을 계산하여 'non_korean_ratio' 열에 추가했습니다.")
 
+    def calculate_noise_ratio_for_hard(self, text):
+        allowed_terms = [
+            'SERICEO', 'APTLD', 'K 시리즈', 'KAIST', 'MWC19', 'NBIoT', 'U파손도움', 'VR·AR', 'u3000', '갤럭시A9', '갤럭시S8', '아이폰XR', '태블릿PC', 
+            'ALCS', 'ETRI', 'FARC', 'GSMA', 'H7N9', 'KISA', 'OECD', 'RCEP', 'S400', 'S500', 'K시리즈', 'T멤버십', 'UCLA', 'UEFA', 'USTR', 'WNBA', 'ai.x', 
+            'e스포츠', '스마트X', '아이폰X', '67P', '75t', 'ACM', 'AFC', 'APT', 'ATM', 'AWS', 'AfD', 'A매치', 'B2B', 'B52', 'BMW', 'BeY', 'CBS', 'CCO', 'CEO', 'CPU', 'CSD', 
+            'DMZ', 'EBS', 'ELS', 'EPL', 'ETF', 'F35', 'FFP', 'G20', 'G20', 'HDC', 'IBK', 'IBM', 'ICT', 'IPO', 'IRP', 'ISA', 'IoT', 'KAT', 'KBO', 'KBO', 'KBS', 'KEB', 'KGC', 'KTB', 'K리그', 
+            'LGU', 'LNG', 'LPG', 'LTE', 'MBC', 'MLB', 'MMT', 'MOU', 'MVP', 'MWC', 'NBA', 'NEW', 'NHK', 'NHN', 'NSA', 'N여행', 'PVC', 'RFA', 'S10', 'SBS', 'SDI', 'SKB', 'SKT', 'SKT', 'SNS', 
+            'TBN', 'TPP', 'U19', 'U20', 'UAE', 'UAR', 'UCC', 'URL', 'USC', 'V30', 'V50', 'V리그', 'WON', 'WTI', 'WTO', 'aix', 'com', 'iOS', 'tbs', '채널A', '%P', '%p', '3D', '3Q', '5G', 
+            'A8', 'AG', 'AI', 'AS', 'Be', 'CJ', 'C조', 'D8', 'DB', 'EM', 'ES', 'EU', 'FA', 'FC', 'FK', 'G6', 'G7', 'G8', 'GB', 'GS', 'HF', 'IC', 'IP', 'IS', 'IT', 'KB', 'KT', 'K리', 'K콘', 
+            'LA', 'LD', 'LG', 'LH', 'LS', 'ML', 'MS', 'NC', 'NH', 'NO', 'OK', 'PC', 'PF', 'PO', 'PS', 'RD', 'S9', 'SA', 'SK', 'ST', 'SW', 'S펜', 'TI', 'TK', 'TV', 'T맵', 'VR', 'VS', 'WS', 'X6', 'XR', 
+            'kt', 'vs', ',', '.', 'm', '|', '·', '…', '⅔', '↑', '→', '↓', '↔', '∼', '③', '④', 'ㆍ', '㎜', '㎝', '㎡', '＋', 'ｍ', '2％'
+        ]
+        text = text.replace(" ", "")
+        for term in allowed_terms:
+            text = text.replace(term, "")
+        text = re.sub(r'(?<=\d)%', '', text)
+        non_korean_text = re.sub(r'[가-힣0-9\u4e00-\u9fff]', '', text)
+        return len(non_korean_text) / len(text) if len(text) > 0 else 0
+
     def read_data(self):
-        """
-        CSV 파일을 읽어옵니다.
-        """
         try:
             self.df = pd.read_csv(self.input_file, encoding='utf-8')
             logging.info(f"입력 파일 '{self.input_file}'을 성공적으로 읽었습니다.")
@@ -81,37 +71,7 @@ class Noise_isolation:
             logging.error(f"파일을 읽는 중 오류가 발생했습니다: {e}")
             raise
 
-    def clean_data(self):
-        """
-        데이터 전처리를 수행합니다.
-        """
-        # 'text' 열의 NaN 값을 빈 문자열로 대체
-        self.df['text'] = self.df['text'].fillna('')
-        # 'text' 열의 앞뒤 공백 제거
-        self.df['text'] = self.df['text'].str.strip()
-        # '...'을 '…'으로 대체
-        self.df['text'] = self.df['text'].apply(self.replace_ellipsis)
-
-    def process_data(self):
-        """
-        데이터 처리 및 필터링을 수행합니다.
-        """
-        # 'non_kor' 열에 패턴에 매칭되는 문자들만 추출
-        self.df['non_kor'] = self.df['text'].apply(self.extract_non_korean_chars)
-        # 한국어가 아닌 문자 비율 계산
-        self.calculate_non_korean_ratio_vectorized()
-        # 지정된 비율 범위에 해당하는 데이터 필터링
-        self.filtered_df = self.df[
-            (self.df['non_korean_ratio'] > self.non_korean_ratio_lower_threshold) &
-            (self.df['non_korean_ratio'] <= self.non_korean_ratio_upper_threshold)
-        ]
-        logging.info(f"한국어가 아닌 문자 비율이 {self.non_korean_ratio_lower_threshold} 이상 {self.non_korean_ratio_upper_threshold} 이하인 문장을 필터링했습니다. 총 {len(self.filtered_df)}개의 문장이 추출되었습니다.")
-
     def save_data(self):
-        """
-        필터링된 데이터를 CSV 파일로 저장합니다.
-        """
-        # 출력 디렉토리 생성
         output_dir = os.path.dirname(self.output_file)
         os.makedirs(output_dir, exist_ok=True)
 
@@ -122,17 +82,46 @@ class Noise_isolation:
             logging.error(f"파일을 저장하는 중 오류가 발생했습니다: {e}")
             raise
 
-    def run(self, save=False):
-        """
-        전체 프로세스를 실행합니다.
-        """
+    def clean_data(self):
+        self.df['text'] = self.df['text'].fillna('')
+        self.df['text'] = self.df['text'].str.strip()
+        self.df['text'] = self.df['text'].apply(self.replace_ellipsis) # '...'을 '…'으로 대체
+        self.df['non_kor'] = self.df['text'].apply(self.extract_non_korean_chars) # 'non_kor' 열에 패턴에 매칭되는 문자들만 추출
+
+    def isolate(self, save=False, type='hard'):
         self.read_data()
-        self.clean_data()
-        self.process_data()
+
+        if type == 'soft':
+            self.clean_data() # 데이터 클리닝 
+            self.calculate_non_korean_ratio() 
+            # 지정된 비율 범위에 해당하는 데이터 필터링
+            self.noised_df = self.df[
+                (self.df['non_korean_ratio'] > 0.1) &
+                (self.df['non_korean_ratio'] <= 0.36)
+            ]
+            self.filtered_df = self.df[
+                (self.df['non_korean_ratio'] <= 0.11)
+            ]
+            logging.info(f"노이즈가 포함된 {len(self.noised_df)}개, 노이즈가 제거된 {len(self.filtered_df)}개의 문장이 추출되었습니다.")
+        elif type == 'hard':
+            self.df['noise_ratio'] = self.df['text'].apply(self.calculate_noise_ratio_for_hard)
+
+            bins = [0, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 1]
+            labels = ['0', '0.2', '0.3', '0.4', '0.5', '0.6', '0.7', '0.8+']
+            self.df['noise_group'] = pd.cut(self.df['noise_ratio'], bins=bins, labels=labels, include_lowest=True)
+
+            self.noised_df = self.df[self.df['noise_ratio'] > 0].copy()
+            self.filtered_df = self.df[self.df['noise_ratio'] == 0].copy()
+
+            # self.noised_df[['ID', 'text', 'target']].to_csv("noise_train.csv", index=False)
+            # self.filtered_df[['ID', 'text', 'target']].to_csv("clean_train.csv", index=False)
+        else:
+            print("그런건 없습니다.")
+
         if save:
             self.save_data()
 
-        return self.filtered_df
+        return self.noised_df, self.filtered_df
 
 if __name__ == "__main__":
     # 입력 및 출력 파일 경로 설정
@@ -143,15 +132,12 @@ if __name__ == "__main__":
     input_file = os.path.join(DATA_DIR, 'train.csv')
     output_file = os.path.join(OUTPUT_DIR, 'noise_isolated_test.csv')
 
-    # 클래스 인스턴스 생성
-    korean_text_filter = Noise_isolation(
+    noise_isolator = Noise_isolation(
         input_file=input_file,
         output_file=output_file,
-        non_korean_ratio_lower_threshold=-1,  # 필요에 따라 값 조정
-        non_korean_ratio_upper_threshold=0.11   # 필요에 따라 값 조정
     )
 
-    # 프로세스 실행
-    df = korean_text_filter.run(save=True)
+    ndf, fdf = noise_isolator.isolate(type='hard')
 
-    print(df.head())
+    print(ndf.head())
+    print(fdf.head())
