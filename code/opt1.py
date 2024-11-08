8f26309bd0908f7f068d73bbb506459ff141e8d4
Yunseo <126463742+Yunseo-Lab@users.noreply.github.com>
2024-11-08 16:27:14 +0900

    opt1.py 정리

--- code/opt1.py
+++ code/opt1.py
@@ -7,7 +7,7 @@ from relabeling import ReLabelingEnsemble, LabelErrorCorrector
 # 입력 및 출력 파일 경로 설정
 BASE_DIR = os.getcwd()
 DATA_DIR = os.path.join(BASE_DIR, '../data')
-input_file = os.path.join(DATA_DIR, 'small.csv')
+input_file = os.path.join(DATA_DIR, 'train.csv')
 
 noise_isolator = Noise_isolation(
     input_file=input_file,
@@ -16,34 +16,23 @@ noise_isolator = Noise_isolation(
 augmentor = Augmentation(
     output_dir=DATA_DIR
 )
-relabeling_ensemble = ReLabelingEnsemble(
-    model_names=["klue/bert-base"]
-)
+relabeling_ensemble = ReLabelingEnsemble()
 
 # isolation 실행
 noised_df, filtered_df = noise_isolator.isolate(type='hard')
 
-print(noised_df)
-print(filtered_df)
-
 # 노이지 데이터 증강 
 augmented_noised_df = augmentor.naive_augment(noised_df)
-
-print(augmented_noised_df)
 
 # 리라벨링
 relabeled_filtered_df = relabeling_ensemble.run(
     train_path_or_data=augmented_noised_df,
     relabeling_path_or_data=filtered_df,
 )
-print(relabeled_filtered_df)
 
 # 라벨링 된 filtered_df 증강
 augmented_relabeled_filtered_df = augmentor.easy_data_augmentation(relabeled_filtered_df)
-print(augmented_relabeled_filtered_df)
 augmented_relabeled_filtered_df = augmentor.back_translation(relabeled_filtered_df)
-print(augmented_relabeled_filtered_df)
-
 
 # augmented_relabeled_filtered_df + augmented_noised_df and shuffle
 last = pd.concat([augmented_noised_df, augmented_relabeled_filtered_df], ignore_index=True)
